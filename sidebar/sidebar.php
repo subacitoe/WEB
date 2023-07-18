493fceed0200b0c76158b4f540dc3230bb1bedc4
subacitoe <109783591+subacitoe@users.noreply.github.com>
2023-07-18 15:08:15 +0700

    Delete sidebar directory

--- sidebar/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="side_bar">
-    <ul>
-        <li><i class="fa-solid fa-house" style="color: #dbd6d6;"></i><a href="www.google.com">Dashboard</a></li>
-        <li><i class="fa-solid fa-cube"  style="color: #dbd6d6;"></i><a href="#">Product</a></li>
-        <li><i class="fa-solid fa-receipt"  style="color: #dbd6d6;"></i><a href="#">Bill</a></li>
-        <li><i class="fa-solid fa-child"  style="color: #dbd6d6;"></i><a href="#">Customer</a></li>
-        <li><i class="fa-solid fa-calendar-days" style="color: #dbd6d6;"></i><a href="#">Calendar</a></li>
-    </ul>
-</div>
